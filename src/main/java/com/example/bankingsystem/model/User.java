68a47a58673710cce4f94d2e3593483eeb3ba0a2
YOVANM <martinezyovam@gmail.com>
2025-01-15 08:37:42 -0300

    Added salt to hash password with PBKDF2

--- src/main/java/com/example/bankingsystem/model/User.java
+++ src/main/java/com/example/bankingsystem/model/User.java
@@ -35,6 +35,9 @@ public class User {
   @Column(nullable = false, length = 255)
   private String passwordHash;
 
+  @Column(nullable = false, length = 255)
+  private String salt;
+
   @Column(nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
   private LocalDateTime createdAt;
 
